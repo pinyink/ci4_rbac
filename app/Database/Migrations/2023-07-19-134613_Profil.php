f857043baf58dc7fac1aca7df78ea0a380737d2e
pinyink <nurkhafindi@gmail.com>
2023-07-19 20:52:40 +0700

    migration profil

---
+++ app/Database/Migrations/2023-07-19-134613_Profil.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Profil extends Migration
+{
+    public function up()
+    {
+        //
+        $this->forge->addField([
+            'user_id' => [
+                'type' => 'INT',
+                'constraint' => '11',
+                'unsigned' => true
+            ],
+            'profil_firstname' => [
+                'type' => 'VARCHAR',
+                'constraint' => '64',
+                'default' => null
+            ],
+            'profil_lastname' => [
+                'type' => 'VARCHAR',
+                'constraint' => '64',
+                'default' => null,
+            ],
+            'profil_emaul' => [
+                'type' => 'VARCHAR',
+                'constraint' => '64',
+                'default' => null
+            ],
+            'profil_bio' => [
+                'type' => 'TEXT',
+                'default' => null
+            ],
+            'profil_image' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255'
+            ]
+        ]);
+
+        $this->forge->addKey('user_id', true);
+        $this->forge->createTable('profil');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('profil');
+        //
+    }
+}
