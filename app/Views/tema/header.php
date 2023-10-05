39652d6338825e57f5789058424759ea8736204e
pinyink <nurkhafindi@gmail.com>
2023-10-05 11:22:34 +0700

    add theme orange custom

--- app/Views/tema/header.php
+++ app/Views/tema/header.php
@@ -136,7 +136,7 @@
             <li class="dropdown dropdown-user">
                 <a class="nav-link dropdown-toggle link" data-toggle="dropdown">
                     <img src="<?= session('profil_image'); ?>" class="imgProfil" />
-                    <span></span><?= session('fullname'); ?><i class="fa fa-angle-down m-l-5"></i></a>
+                    <span><?= session('fullname'); ?></span><i class="fa fa-angle-down m-l-5"></i></a>
                 <ul class="dropdown-menu dropdown-menu-right">
                     <a class="dropdown-item" href="<?= base_url(); ?>/setting/profil"><i class="fa fa-user"></i>Profil</a>
                     <li class="dropdown-divider"></li>
